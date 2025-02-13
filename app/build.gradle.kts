a7ae0fd50919be87f041f1246fc3f2e16519febc
Alex Barclay <adbarclay92@gmail.com>
2025-02-12 19:20:26 -0700

    It runs now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.openbarclay.readqueue"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.openbarclay.readqueue"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
